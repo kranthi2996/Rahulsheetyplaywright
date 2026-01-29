91f90378796d9a5fb3a2a65e84a1dffe23ff7c39
Kranthi Kumar <kranthedu27@gmail.com>
2026-01-29 09:16:00 +0530

    apiweb

--- tests/webapi.spec.js
+++ tests/webapi.spec.js
@@ -1,40 +1,46 @@
-const {test, expect, request} = require('@playwright/test');4
-const loginPayload= {userEmail: "anshika@gmail.com", userPassword: "Iamking@000"};
-
-test.beforeAll(async()=>{
-
-    const apiContext= await request.newContext();
-    const loginResponse= await apiContext.post('https://rahulshettyacademy.com/api/ecom/auth/login',{
-        data:loginPayload
-    });
-    expect(loginResponse.ok()).toBeTruthy();
-    const loginResponseJson= await loginResponse.json();
-    global.token= loginResponseJson.token;         
-
-
+const {test, expect, request} = require('@playwright/test');
+const {APiUtils} = require('./utils/APiUtils');
+const loginPayLoad = {userEmail:"anshika@gmail.com",userPassword:"Iamking@000"};
+const orderPayLoad = {orders:[{country:"Cuba",productOrderedId:"67a8dde5c0d3e6622a297cc8"}]};
+ 
+ 
+let response;
+test.beforeAll( async()=>
+{
+   const apiContext = await request.newContext();
+   const apiUtils = new APiUtils(apiContext,loginPayLoad);
+   response =  await apiUtils.createOrder(orderPayLoad);
+ 
+})
+ 
+ 
+//create order is success
+test.only('@API Place the order', async ({page})=>
+{ 
+    await page.addInitScript(value => {
+ 
+        window.localStorage.setItem('token',value);
+    }, response.token );
+await page.goto("https://rahulshettyacademy.com/client");
+ await page.locator("button[routerlink*='myorders']").click();
+ await page.locator("tbody").waitFor();
+const rows = await page.locator("tbody tr");
+ 
+ 
+for(let i =0; i<await rows.count(); ++i)
+{
+   const rowOrderId =await rows.nth(i).locator("th").textContent();
+   if (response.orderId.includes(rowOrderId))
+   {
+       await rows.nth(i).locator("button").first().click();
+       break;
+   }
+}
+const orderIdDetails =await page.locator(".col-text").textContent();
+//await page.pause();
+expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
+ 
 });
-
-test('place order from API', async ({page}) => {
-
-    //set storage state with token
-    page.addInitScript( token => {
-        window.localStorage.setItem('token', token);
-    }, global.token);   
-    const email = "";
-    const productName = 'ZARA COAT 4';
-    await page.goto("https://rahulshettyacademy.com/client");
-        const products = page.locator(".card-body");
-         const titles = await page.locator(".card-body b").allTextContents();
-         console.log(titles); 
-         const count = await products.count();  
-            for (let i = 0; i < count; ++i) {
-                if (await products.nth(i).locator("b").textContent() === productName) {
-
-                     //add to cart
-                        await products.nth(i).locator("text= Add To Cart").click();
-                        break;
-                }
-            }
-        }
-    );
-
+ 
+//Verify if order created is showing in history page
+// Precondition - create order -
